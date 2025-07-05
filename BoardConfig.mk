985cdd22cd3e586440e8f85e49c6c8868aa78747
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-06 02:48:40 +0800

    snowcastle: Specify image mounts on fstab
    Requires:
    - https://review.lineageos.org/c/LineageOS/android_system_core/+/436344
    - https://review.lineageos.org/c/LineageOS/android_system_core/+/436343
    
    Change-Id: I501471d3ac7335fc060fce00a9f36196d390d6a1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,7 @@ BOARD_KERNEL_CMDLINE := \
     $(MAINLINE_COMMON_ANDROIDBOOT_PARAMS) \
     $(MAINLINE_COMMON_KERNEL_PARAMS) \
     androidboot.hardware=snowcastle \
+    androidboot.partition_map=nvme0n1p1,base \
     androidboot.use_tmpfs_userdata=1 \
     androidboot.verifiedbootstate=orange \
     console=tty0
